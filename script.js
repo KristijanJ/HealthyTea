a20a15bf992998b8dd715adbbd0213cb020f702a
Kristijan Jovanovski <kristijan.j92@gmail.com>
2019-11-03 20:20:41 +0100

    menu-fix & new images

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 const hamburger = document.getElementById("hamburger");
 const nav = document.querySelector(".nav");
 
-hamburger.addEventListener("click", () => {
-  if (nav.classList.contains("active")) {
-    nav.classList.remove("active");
-  } else {
-    nav.classList.add("active");
-  }
-});
+hamburger.addEventListener("click", () => nav.classList.toggle("open"));
